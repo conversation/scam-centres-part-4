9daebd5230713e4bafdaffb3fd4e42f1034549cf
Matt Garrow <mjkgarrow@gmail.com>
2025-01-08 15:10:40 +1100

    fix: implemented embeded tweets

---
+++ src/components/Tweet1.tsx
@@ -0,0 +1,19 @@
+export default function Tweet1() {
+  return (
+    <div className="w-[min(100%-40px,750px)]  flex justify-center">
+      <blockquote className="twitter-tweet">
+        <p lang="en">
+          40 Vietnamese men and women fled a casino in Cambodia on Thursday and
+          tried to swim back to Vietnam&#39;s An Giang province. One reportedly
+          drowned. They said they were duped into working illegally Cambodia and
+          got beaten.{" "}
+          <a href="https://t.co/Q9UHpVbaYh">pic.twitter.com/Q9UHpVbaYh</a>
+        </p>
+        &mdash; Nga Pham (@ngahpham){" "}
+        <a href="https://twitter.com/ngahpham/status/1560508973006360576?ref_src=twsrc%5Etfw">
+          August 19, 2022
+        </a>
+      </blockquote>{" "}
+    </div>
+  );
+}
---
+++ src/components/Tweet2.tsx
@@ -0,0 +1,20 @@
+export default function Tweet2() {
+  return (
+    <div className="w-[min(100%-40px,750px)] flex justify-center">
+      <blockquote className="twitter-tweet">
+        <p lang="en">
+          More than 70 Vietnamese fled a Chinese-run casino in Svay Rieng
+          Cambodia on 17 Sept. This is the second incident in two months. In
+          August 42 Vietnamese workers swam across a river between Vietnam and
+          Cambodia after being chased by casino guards. One drowned.{" "}
+          <a href="https://t.co/ssTKp1dFvN">pic.twitter.com/ssTKp1dFvN</a>
+        </p>
+        &mdash; Nga Pham (@ngahpham){" "}
+        <a href="https://twitter.com/ngahpham/status/1572180278290034688?ref_src=twsrc%5Etfw">
+          September 20, 2022
+        </a>
+      </blockquote>{" "}
+      {/* <script async src="https://platform.twitter.com/widgets.js"></script> */}
+    </div>
+  );
+}
