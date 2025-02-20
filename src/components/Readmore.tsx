c041a29e5085fc9fc0c153bb4593706af3195405
Matt Garrow <mjkgarrow@gmail.com>
2025-02-20 15:26:49 +1100

    edit: readmore text changes

--- src/components/Readmore.tsx
+++ src/components/Readmore.tsx
@@ -1,20 +1,38 @@
-export default function Readmore({
-  url,
-  description,
-  linkText,
-}: {
-  url: string;
-  description: string;
-  linkText: string;
-}) {
+export default function Readmore() {
   return (
-    <p className="my-10 border-y border-neutral-500 py-4 italic">
-      <span className="">
-        Readmore: {description}{" "}
-        <a href={url} target="_parent" className="text-neutral-200">
-          {linkText}
+    <div className="relative my-10 grid grid-cols-[minmax(5rem,10%)_1fr] gap-4 border-y border-neutral-500 py-4 sm:grid-cols-[minmax(8rem,10%)_1fr]">
+      <div className="not-prose aspect-square w-full overflow-hidden rounded-md">
+        <a
+          href="#"
+          target="_parent"
+          className="text-neutral-100 hover:text-neutral-200"
+        >
+          <img
+            src="https://images.theconversation.com/files/645965/original/file-20250130-15-bubzm1.png?ixlib=rb-4.1.0&q=20&auto=format&w=128"
+            alt=""
+            className="not_fullscreen_media h-32 w-32 max-sm:h-20 max-sm:w-20"
+          />
         </a>
-      </span>
-    </p>
+      </div>
+      <p className="!mb-0 !mt-0 italic">
+        <a
+          href="#"
+          target="_parent"
+          className="text-neutral-100 hover:text-neutral-200"
+        >
+          Scam Factories
+        </a>{" "}
+        is a special multimedia and podcast series by The Conversation. Listen
+        to the{" "}
+        <a
+          href="#"
+          target="_parent"
+          className="text-neutral-100 hover:text-neutral-200"
+        >
+          podcast series here
+        </a>
+        .
+      </p>
+    </div>
   );
 }
